2911a73ec42ab44cb7bae323abb9782b0c32ad99
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-03-11 20:23:18 +0200

    Create Garantee section

---
+++ shared/components/guarantees/Guarantees.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Guarantees.module.css";
+
+const Guarantees = () => {
+  return <div className={styles.container}>Guarantees</div>;
+};
+
+export default Guarantees;
