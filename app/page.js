b3b5a5b90283d5cc4659a5f69636285598aea2ec
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-03-13 21:05:10 +0200

    add projects edits

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Header from "@/shared/components/Header/Header";
 import MainScreen from "@/shared/components/MainScreen/MainScreen";
 import About from "@/shared/components/About/About";
 import Guarantees from "@/shared/components/Guarantees/Guarantees";
+import Projects from "@/shared/components/Projects/Projects";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <MainScreen />
         <Guarantees />
         <About />
+        <Projects />
       </main>
     </>
   );
