1c700fed70fe05209014a2ead6bd425299f21cb7
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-03-17 18:42:48 +0200

    Create sendmail api

---
+++ app/api/sendMail/route.js
@@ -0,0 +1,50 @@
+import nodemailer from "nodemailer";
+
+export async function POST(req) {
+  const { name, email, phone, comment } = await req.json();
+
+  console.log(name, email, phone, comment);
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: "vlad.ustimenko@gmail.com",
+      subject: `Заявка LUdevelopment від ${name}`,
+      html: `
+			<html>
+				<body>
+					<div style="font-family: Arial, sans-serif;">
+						<h1 style="color: #3b4a8b;">Нова заявка з сайту LineUp Development</h1>
+						<p><strong>Ім'я:</strong> ${name}</p>
+						<p><strong>Email:</strong> ${email}</p>
+						<p><strong>Телефон:</strong> ${phone}</p>
+						<p><strong>Коментар:</strong> ${comment}</p>
+					</div>
+				</body>
+			</html>
+		`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return new Response(JSON.stringify({ message: "Лист надіслано!" }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Помилка при відправці листа:", error);
+    return new Response(
+      JSON.stringify({ message: "Не вдалося надіслати лист" }),
+      { status: 500 }
+    );
+  }
+}
