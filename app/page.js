6f78050c2ddbeda2251137616d16936e291f8497
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-03-17 20:28:21 +0200

    moved the footer

--- app/page.js
+++ app/page.js
@@ -21,8 +21,8 @@ export default function Home() {
         <Projects />
         <Services />
         <Contacts />
-        <Footer />
       </main>
+      <Footer />
       <Menu />
     </>
   );
