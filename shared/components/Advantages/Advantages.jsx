eb8c53fe4e3ec9c81e1897256af74bf8a9718b06
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-03-19 20:31:07 +0200

    Create advantages section

---
+++ shared/components/Advantages/Advantages.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./Advantages.module.css";
+import SectionTitle from "../SectionTitle/SectionTitle";
+import Image from "next/image";
+
+const AdvantagesItem = [
+  {
+    imageSrc: "/images/advantages/2.jpg",
+    text: "Виконання робіт згідно з ДБН та рекомендаціями ДСТУ",
+  },
+  {
+    imageSrc: "/images/advantages/4.jpg",
+    text: "Швидка зміна проектних рішень та погодження у відповідних інстанціях ",
+  },
+  {
+    imageSrc: "/images/advantages/6.jpg",
+    text: "Оцінка проектних рішень та надання рекомендацій",
+  },
+  {
+    imageSrc: "/images/advantages/3.jpg",
+    text: "Живучість об’єкту – захист об’єкту від уламків збитих дронів та ракет ",
+  },
+  {
+    imageSrc: "/images/advantages/5.jpg",
+    text: "Висококваліфіковані інженери з багаторічним досвідом в будівництві",
+  },
+  {
+    imageSrc: "/images/advantages/1.webp",
+    text: "Використання передових технологій в будівництві",
+  },
+];
+
+const Advantages = () => {
+  return (
+    <div className={styles.container} id="advantages">
+      <SectionTitle
+        title="Наші переваги"
+        number="03"
+        lineColor="var(--secondAccent)"
+        color="white"
+      />
+      <div className={styles.contentWrapper}>
+        <h2 className={styles.title}>Обираючи нас ви отримуєте</h2>
+        <div className={styles.advantagesGrid}>
+          {AdvantagesItem.map((item) => (
+            <div key={item.text} className={styles.imageWrapper}>
+              <Image
+                src={item.imageSrc}
+                alt="advantages"
+                width={500}
+                height={500}
+                className={styles.image}
+              ></Image>
+              <p className={styles.text}>{item.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Advantages;
