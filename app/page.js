6f049f6696f76262c0d2415104e8c97df019ec72
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-03-31 20:46:15 +0300

    Add dynamic on main page

--- app/page.js
+++ app/page.js
@@ -1,16 +1,6 @@
 import Header from "@/shared/components/Header/Header";
 import MainScreen from "@/shared/components/MainScreen/MainScreen";
-// import About from "@/shared/components/About/About";
-// import Guarantees from "@/shared/components/Guarantees/Guarantees";
-// import Projects from "@/shared/components/Projects/Projects";
-// import Feedback from "@/shared/components/Feedback/Feedback";
 import Menu from "@/shared/components/Menu/Menu";
-// import Services from "@/shared/components/Services/Services";
-// import Contacts from "@/shared/components/Contacts/Contacts";
-import Footer from "@/shared/components/Footer/Footer";
-// import Advantages from "@/shared/components/Advantages/Advantages";
-// import Calculator from "@/shared/components/Calculator/Calculator";
-import Modal from "@/shared/components/Modal/Modal";
 import dynamic from "next/dynamic";
 
 const Projects = dynamic(() => import("@/shared/components/Projects/Projects"));
@@ -19,6 +9,8 @@ const Guarantees = dynamic(() =>
 );
 const About = dynamic(() => import("@/shared/components/About/About"));
 const Feedback = dynamic(() => import("@/shared/components/Feedback/Feedback"));
+const Modal = dynamic(() => import("@/shared/components/Modal/Modal"));
+const Footer = dynamic(() => import("@/shared/components/Footer/Footer"));
 const Services = dynamic(() => import("@/shared/components/Services/Services"));
 const Contacts = dynamic(() => import("@/shared/components/Contacts/Contacts"));
 const Advantages = dynamic(() =>
